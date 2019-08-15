ea2524f0d3a1288dcbe47205bd3ac286ee766ab4
nancy27vazquez <nevadg@outlook.com>
2019-08-15 01:22:24 -0500

    Logic basics and canvas basic path created

--- starter_code/canvas.js
+++ starter_code/canvas.js
@@ -1,32 +1,78 @@
+var canvas = document.getElementById("hangman");
+var ctx = canvas.getContext("2d");
 
-function HangmanCanvas(secretWord) {
-  this.ctx = document.getElementById('hangman').getContext('2d');
+// Hang structure
+ctx.lineWidth = 3;
+ctx.beginPath(0, 0);
+ctx.moveTo(150, 660);
+ctx.lineTo(90, 715);
+ctx.lineTo(210, 715);
+ctx.lineTo(150, 660);
+ctx.stroke();
+
+ctx.beginPath(150, 660);
+ctx.moveTo(150, 660);
+ctx.lineTo(150, 100);
+ctx.lineTo(450, 100);
+ctx.lineTo(450, 150);
+ctx.stroke();
+
+// Poor man's head
+ctx.beginPath();
+ctx.arc(450, 200, 50, 0, Math.PI * 2);
+ctx.lineWidth = 3;
+ctx.stroke();
+ctx.closePath();
+
+// Poor man's body
+ctx.beginPath(0, 0);
+ctx.moveTo(450, 250);
+ctx.lineTo(450, 450);
+ctx.stroke();
+
+// Poor man's right arm
+ctx.beginPath();
+ctx.moveTo(450, 290);
+ctx.lineTo(380, 370);
+ctx.stroke();
+
+// Poor man's left arm
+ctx.beginPath();
+ctx.moveTo(450, 290);
+ctx.lineTo(520, 370);
+ctx.stroke();
+
+// Poor man's right leg
+ctx.beginPath();
+ctx.moveTo(450, 450);
+ctx.lineTo(370, 570);
+ctx.stroke();
+
+// Poor man's left leg
+ctx.beginPath();
+ctx.moveTo(450, 450);
+ctx.lineTo(520, 570);
+ctx.stroke();
+
+// Secret word lines
+ctx.
+
+/* ============= */
+//We also want to store in a variable the secret word.
+
+class HangmanCanvas {
+  constructor(secretWord){
+    this.ctx = document.getElementById("hangman").getContext("2d");
+    this.secretWord = secretWord;
+  }
+  createBoard(){
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+  drawLines(){}
+  writeCorrectLetter(index){}
+  writeWrongLetter(letter, errorsLeft){}
+  drawHangman(){}
+  gameOver(){}
+  winner(){}
 }
-
-HangmanCanvas.prototype.createBoard = function () {
-
-};
-
-HangmanCanvas.prototype.drawLines = function () {
-
-};
-
-HangmanCanvas.prototype.writeCorrectLetter = function (index) {
-
-};
-
-HangmanCanvas.prototype.writeWrongLetter = function (letter, errorsLeft) {
-
-};
-
-HangmanCanvas.prototype.drawHangman = function (shape) {
-
-};
-
-HangmanCanvas.prototype.gameOver = function () {
-
-};
-
-HangmanCanvas.prototype.winner = function () {
-
-};
--- starter_code/hangman.js
+++ starter_code/hangman.js
@@ -1,24 +1,44 @@
-var hangman;
+let hangman;
 
-// function Hangman() {
+class Hangman {
+  constructor() {
+    this.words = ["hola", "nan", "ernesto"];
+    this.secretWord = "";
+    this.letters = [];
+    this.guessedLetter = "";
+    this.errorsLeft = 10;
+  }
+  getWord() {
+    let randomWord = Math.floor(Math.random() * this.words.length);
+    this.secretWord = this.words[randomWord];
+    return this.words[randomWord];
+  }
+  checkIfLetter(a) {
+    if (a <= 65 && a >= 90) {
+      this.letters.push(a);
+      return true;
+    } else {
+      return false;
+    }
+  }
+  checkClickedLetters(a) {
+    if (this.letters.includes(a)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  addCorrectLetter() {}
+  checkGameOver() {
+    if (this.errorsLeft === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-// }
-
-// Hangman.prototype.getWord = function () {
-
-// };
-
-// Hangman.prototype.checkIfLetter = function (keyCode) {
-
-// };
-
-// Hangman.prototype.checkClickedLetters = function (key) {
-
-// };
-
-// Hangman.prototype.addCorrectLetter = function (i) {
-
-// };
+  checkWinner() {}
+}
 
 // Hangman.prototype.addWrongLetter = function (letter) {
 
@@ -32,11 +52,8 @@ var hangman;
 
 // };
 
-document.getElementById('start-game-button').onclick = function () {
+document.getElementById("start-game-button").onclick = function() {
   hangman = new Hangman();
 };
 
-
-document.onkeydown = function (e) {
-
-};
+document.onkeydown = function(e) {};
